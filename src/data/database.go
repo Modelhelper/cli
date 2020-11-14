fa228015176595366962acb951673ee03abe48b3
Hans-Petter Eitvet <hans-petter@patogen.no>
2020-11-14 14:14:48 +0100

    Add interface for database source

--- src/data/database.go
+++ src/data/database.go
@@ -1,1 +1,23 @@
 package data
+
+import (
+	"database/sql"
+	"modelhelper/cli/types"
+)
+
+type Database interface {
+	Connect(source string) (*sql.DB, error)
+
+	Entity(name string) (*types.Entity, error)
+	Entities(pattern string) (*[]types.Entity, error)
+
+	Columns(entityName string) (*[]types.EntityColumn, error)
+
+	ParentRelations(entityName string) (*[]types.EntityRelation, error)
+	ChildRelations(entityName string) (*[]types.EntityRelation, error)
+
+	Indexes(entityName string) (*[]types.EntityIndex, error)
+}
+
+type Optimize interface {
+}
