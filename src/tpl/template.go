c756c817d40ec13f3ba7a3bc1e26e0c92ffa3eda
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-04-15 22:00:45 +0200

    Load templates for nesting

--- src/tpl/template.go
+++ src/tpl/template.go
@@ -2,6 +2,11 @@ package tpl
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -12,6 +17,18 @@ const (
 	ProjectScope               = "project"
 )
 
+type Handler interface {
+	GetBlock()
+	GetSnippets()
+	GetTemplates()
+}
+
+type Generator interface {
+	Generate(model interface{}) (string, error)
+}
+
+type TemplateHandler struct {
+}
 type Template struct {
 	Name            string
 	Version         string
@@ -46,17 +63,122 @@ var (
 	}
 )
 
-type Generator interface {
-	Generate(model interface{}) (string, error)
-}
-
 func (t *Template) Generate(model interface{}) (string, error) {
+	blocks := []*Template{
+		testBlockLvl1(),
+		testBlockLvl2(),
+		t,
+	}
 
-	tpl := template.Must(template.New(t.Language).Parse(t.Body))
+	dir := createTestDir(blocks)
+	// defer os.RemoveAll(dir)
+
+	pattern := filepath.Join(dir, "*")
+
+	drivers := template.Must(template.ParseGlob(pattern))
+
 	buf := new(bytes.Buffer)
+	err := drivers.ExecuteTemplate(buf, t.Name, model)
+
+	// blockTemplates := []*template.Template{} +".tmpl"
+
+	// for _, block := range blocks {
+	// 	blockTemplate, err := template.New(block.Name).Parse(block.Body)
+	// 	if err != nil {
+	// 		continue
+	// 	}
+
+	// 	blockTemplates = append(blockTemplates, blockTemplate)
+	// }
+
+	// tpl, err := template.New(t.Language).Parse(t.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//tpl := template.Must(template.New(t.Language).Parse(t.Body))
 
 	// buf.String() // returns a string of what was written to it
-	tpl.Execute(buf, model)
+	// tpl.Execute(buf, model)
 
 	return buf.String(), nil
+}
+
+func oneMoreTest() *Template {
+	t := Template{}
+
+	t.Language = "poco"
+	t.Body = testTemplate()
+
+	return &t
+}
+
+func testBlockLvl1() *Template {
+
+	t := Template{}
+
+	t.Name = "classname"
+	t.Body = `{{ .Name }}`
+
+	return &t
+}
+
+func testBlockLvl2() *Template {
+
+	t := Template{}
+
+	t.Name = "namespace"
+	t.Body = `namespace {{ .NameSpace }}`
+
+	return &t
+}
+
+func createTestDir(files []*Template) string {
+
+	//err := os.Mkdir("t", 0777)
+	fmt.Println(os.TempDir())
+
+	dir, err := ioutil.TempDir("", "template") //os.MkdirTemp("", "template")
+	fmt.Println(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fp := filepath.Join(dir, file.Name)
+		fmt.Println(fp)
+		err := ioutil.WriteFile(fp, []byte(file.Body), 0777)
+		// f, err := os.Create(fp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// defer f.Close()+".tmpl"
+		// _, err = io.WriteString(f, file.Body)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+	}
+	return dir
+}
+
+func testTemplate() string {
+	return `
+// code her - indent by SPACE not TAB
+{{- $model := index .Code.Types "model" }}
+using System;
+{{- range .Code.Imports }}
+{{.}}
+{{- end }}
+
+
+//namespace from template
+namespace {{ $model.NameSpace }} 
+{	
+	{{ template "block0" . }}
+	public class {{ .Name }}{{ $model.NamePostfix}} 
+	{
+
+	}
+}
+
+{{.}}
+`
 }
