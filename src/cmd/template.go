3ce7d92966f51929b4ea51a7f733ec03a143f839
hp-e <hpephoto@gmail.com>
2021-05-14 00:56:24 +0200

    Move filtering and grouping to tpl package

--- src/cmd/template.go
+++ src/cmd/template.go
@@ -57,6 +57,7 @@ Filter the template by using on or more of the following options
 	
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
+
 		cfg := config.Load()
 
 		tl := tpl.TemplateLoader{
@@ -64,40 +65,62 @@ Filter the template by using on or more of the following options
 		}
 		allTemplates, _ := tl.LoadTemplates()
 
+		if len(args) > 0 {
+			current, found := allTemplates[args[0]]
+			if found {
+				fmt.Print(current.ToString(args[0]))
+			}
+
+			return
+		}
 		group, _ := cmd.Flags().GetString("by")
 		typeFiler, _ := cmd.Flags().GetStringArray("type")
 		langFilter, _ := cmd.Flags().GetStringArray("lang")
 		modelFilter, _ := cmd.Flags().GetStringArray("model")
 		keyFilter, _ := cmd.Flags().GetStringArray("key")
+		groupFilter, _ := cmd.Flags().GetStringArray("group")
 
 		var tp templatePrinter
 		if len(typeFiler) > 0 {
-			ft := filterByType{}
-			allTemplates = ft.filter(allTemplates, typeFiler)
+			ft := tpl.FilterByType{}
+			allTemplates = ft.Filter(allTemplates, typeFiler)
 		}
 
 		if len(langFilter) > 0 {
-			ft := filterByLang{}
-			allTemplates = ft.filter(allTemplates, langFilter)
+			ft := tpl.FilterByLang{}
+			allTemplates = ft.Filter(allTemplates, langFilter)
 		}
 		if len(keyFilter) > 0 {
-			ft := filterByKey{}
-			allTemplates = ft.filter(allTemplates, keyFilter)
+			ft := tpl.FilterByKey{}
+			allTemplates = ft.Filter(allTemplates, keyFilter)
 		}
 		if len(modelFilter) > 0 {
-			ft := filterByModel{}
-			allTemplates = ft.filter(allTemplates, modelFilter)
+			ft := tpl.FilterByModel{}
+			allTemplates = ft.Filter(allTemplates, modelFilter)
+		}
+		if len(groupFilter) > 0 {
+			ft := tpl.FilterByGroup{}
+			allTemplates = ft.Filter(allTemplates, groupFilter)
 		}
 
 		if len(group) > 0 {
 			ui.PrintConsoleTitle("ModelHelper Templates grouped by " + group)
 			fmt.Println("In the list below you will find all available templates in ModelHelper\n")
-			grouper := getGrouper(strings.ToLower(group))
-			mg := grouper.group(allTemplates)
+
+			grouper := tpl.GetGrouper(strings.ToLower(group))
+			descr := tpl.GetDescriber(group)
+
+			mg := grouper.Group(allTemplates)
 
 			for typ, tv := range mg {
-				ui.ConsoleTitle(typ)
+				ui.PrintConsoleTitle(typ)
 
+				desc := descr.Describe(typ)
+				if desc != nil {
+					fmt.Println(desc.Long)
+				}
+
+				fmt.Println()
 				tp.templates = tv
 				ui.RenderTable(&tp, &tp)
 
@@ -121,7 +144,8 @@ func init() {
 	templateCmd.Flags().StringArray("type", []string{}, "Filter the templates by the name of the type")
 	templateCmd.Flags().StringArray("lang", []string{}, "Filter the templates by language")
 	templateCmd.Flags().StringArray("model", []string{}, "Filter the templates by model")
-	templateCmd.Flags().StringArray("key", []string{}, "Filter the templates by model")
+	templateCmd.Flags().StringArray("key", []string{}, "Filter the templates by key")
+	templateCmd.Flags().StringArray("group", []string{}, "Filter the templates by group")
 
 }
 
@@ -139,6 +163,7 @@ func (t *templatePrinter) ToRows() [][]string {
 			t.Language,
 			t.Type,
 			t.Model,
+			t.Key,
 			groups,
 			t.Short,
 		}
@@ -156,277 +181,10 @@ func (t *templatePrinter) BuildHeader() []string {
 		"Language",
 		"Type",
 		"Model",
+		"Key",
 		"Groups",
 		"Description",
 	}
 
 	return row
-}
-
-type templateGrouper interface {
-	group(t map[string]tpl.Template) map[string]map[string]tpl.Template
-}
-
-func getGrouper(t string) templateGrouper {
-
-	switch t {
-
-	case "language":
-		return &groupByLanguage{}
-	case "group":
-		return &groupByGroup{}
-	case "key":
-		return &groupByKey{}
-	case "model":
-		return &groupByModel{}
-	case "tag":
-		return &groupByTag{}
-	default:
-		return &groupByType{}
-
-	}
-}
-
-type groupByType struct{}
-type groupByGroup struct{}
-type groupByTag struct{}
-type groupByLanguage struct{}
-type groupByKey struct{}
-type groupByModel struct{}
-
-func (g *groupByType) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		k, f := m[template.Type]
-
-		if !f {
-			k = make(map[string]tpl.Template)
-		}
-
-		k[tn] = template
-		m[template.Type] = k
-
-	}
-
-	return m
-}
-func (g *groupByKey) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-	empty := make(map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		if len(template.Key) == 0 {
-			empty[tn] = template
-		} else {
-
-			k, f := m[template.Key]
-
-			if !f {
-				k = make(map[string]tpl.Template)
-			}
-
-			k[tn] = template
-			m[template.Key] = k
-		}
-
-	}
-
-	if len(empty) > 0 {
-		m["nokey"] = empty
-	}
-
-	return m
-}
-func (g *groupByModel) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-	empty := make(map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		if len(template.Model) == 0 {
-			empty[tn] = template
-		} else {
-
-			k, f := m[template.Model]
-
-			if !f {
-				k = make(map[string]tpl.Template)
-			}
-
-			k[tn] = template
-			m[template.Model] = k
-		}
-
-	}
-
-	if len(empty) > 0 {
-		m["empty"] = empty
-	}
-
-	return m
-}
-
-func (g *groupByLanguage) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-	empty := make(map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		if len(template.Language) > 0 {
-			empty[tn] = template
-		} else {
-
-			if template.Type != "block" {
-
-				k, f := m[template.Language]
-
-				if !f {
-					k = make(map[string]tpl.Template)
-				}
-
-				k[tn] = template
-				m[template.Language] = k
-			}
-		}
-	}
-
-	if len(empty) > 0 {
-		m["empty"] = empty
-	}
-
-	return m
-}
-func (g *groupByTag) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-	empty := make(map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		if template.Type != "block" {
-
-			var tmpl = template
-
-			if len(template.Tags) == 0 {
-				empty[tn] = tmpl
-			} else {
-				for _, grp := range template.Tags {
-
-					k, f := m[grp]
-
-					if !f {
-						k = make(map[string]tpl.Template)
-					}
-
-					k[tn] = tmpl
-					m[grp] = k
-				}
-			}
-		}
-
-	}
-
-	if len(empty) > 0 {
-		m["empty"] = empty
-	}
-	return m
-}
-func (g *groupByGroup) group(t map[string]tpl.Template) map[string]map[string]tpl.Template {
-	m := make(map[string]map[string]tpl.Template)
-	empty := make(map[string]tpl.Template)
-
-	for tn, template := range t {
-
-		if template.Type != "block" {
-
-			var tmpl = template
-
-			if len(template.Groups) == 0 {
-				empty[tn] = tmpl
-			} else {
-				for _, grp := range template.Groups {
-
-					k, f := m[grp]
-
-					if !f {
-						k = make(map[string]tpl.Template)
-					}
-
-					k[tn] = tmpl
-					m[grp] = k
-				}
-			}
-		}
-	}
-
-	if len(empty) > 0 {
-		m["empty"] = empty
-	}
-
-	return m
-}
-
-type templateFilter interface {
-	filter(t map[string]tpl.Template, filter []string) map[string]tpl.Template
-}
-type filterByLang struct{}
-type filterByType struct{}
-type filterByModel struct{}
-type filterByKey struct{}
-
-func (f *filterByType) filter(t map[string]tpl.Template, filter []string) map[string]tpl.Template {
-	output := make(map[string]tpl.Template)
-
-	for name, template := range t {
-		if contains(filter, template.Type) {
-			output[name] = template
-		}
-	}
-
-	return output
-}
-func (f *filterByLang) filter(t map[string]tpl.Template, filter []string) map[string]tpl.Template {
-	output := make(map[string]tpl.Template)
-
-	for name, template := range t {
-		if contains(filter, template.Language) {
-			output[name] = template
-		}
-	}
-
-	return output
-}
-func (f *filterByModel) filter(t map[string]tpl.Template, filter []string) map[string]tpl.Template {
-	output := make(map[string]tpl.Template)
-
-	for name, template := range t {
-		if contains(filter, template.Model) {
-			output[name] = template
-		}
-	}
-
-	return output
-}
-func (f *filterByKey) filter(t map[string]tpl.Template, filter []string) map[string]tpl.Template {
-	output := make(map[string]tpl.Template)
-
-	for name, template := range t {
-		if contains(filter, template.Key) {
-			output[name] = template
-		}
-	}
-
-	return output
-}
-
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-
-	return false
 }
