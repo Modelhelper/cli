b6e77d4831467206e9ec464dd856309da7a6f1ce
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-05-14 11:54:24 +0200

    Add root command for set

--- src/cmd/set.go
+++ src/cmd/set.go
@@ -32,7 +32,38 @@ var setCmd = &cobra.Command{
 	Use:   "set",
 	Short: "Root command to set parameter in the config",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("This is the root command for setting values to the config file. ")
+
+		m := `This is the root command for setting values to the config file. 
+		
+Please use one of the following sub commands to set correct information:
+
+templatelocation | tl    : to set a template location
+developer | dev          : to set developer params
+ 
+samples:
+
+-- Template location
+usage:
+mh set templatelocation [path]
+
+if path argument is blank/empty, the cli assumes you will add current working dir
+
+if mh is executed from /c/dev/modelhelper/t/templates the following command will set '/c/dev/modelhelper/t/templates' 
+as current template location in the config file.
+
+mh set templatelocation
+
+-- Developer info
+usage:
+mh set developer [options]
+
+mh set developer --name your name --email your@email.com
+mh set dev --name name --email email
+
+		`
+
+		fmt.Println(m)
+		fmt.Println("")
 		fmt.Println("Please use one of the sub commands to set correct information")
 	},
 }
