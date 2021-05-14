e2569355cc393535311ff3f88d636c4a663708bb
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-05-14 15:27:23 +0200

    Prompt for an editor to open the config
    Will only prompt if the default editor is not set

--- src/cmd/config.go
+++ src/cmd/config.go
@@ -35,53 +35,24 @@ var configCmd = &cobra.Command{
 	Short: "<not implemented>",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// cfg := modelHelperApp.Configuration
-		// fmt.Println(cfg.AppVersion)
-		// fmt.Println(cfg.ConfigVersion)
-		// fmt.Println(cfg.Languages.Definitions)
-		// fmt.Println(cfg.Templates.Location)
-
 		open, _ := cmd.Flags().GetBool("open")
 		if open {
+			var editor string
+			cfg := config.Load()
+
+			if len(cfg.DefaultEditor) > 0 {
+				editor = cfg.DefaultEditor
+			} else {
+				editor = promptForEditorKey("Please select editor to open the config")
+			}
+
 			loc := filepath.Join(config.Location(), "config.yaml")
-			exe := exec.Command("code", loc)
+			exe := exec.Command(editor, loc)
 			if exe.Run() != nil {
 				//vim didn't exit with status code 0
 			}
 		}
-		// for _, source := range mhConfig.Sources {
-		// 	fmt.Println(source.Name)
 
-		// 	for _, opt := range source.Options {
-		// 		fmt.Println(opt)
-		// 	}
-
-		// 	for _, grp := range source.Groups {
-		// 		for _, itm := range grp.Items {
-
-		// 			fmt.Println(itm)
-		// 		}
-		// 	}
-		// }
-
-		// is := []string{"test_1", "test_2", "test_3"}
-		// g := config.SourceGroup{
-		// 	Items: is,
-		// }
-
-		// gg := make(map[string]config.SourceGroup)
-		// gg["basic"] = g
-
-		// s := config.Source{
-		// 	Name:   "testing",
-		// 	Schema: "dbo",
-		// 	Groups: gg,
-		// }
-		// mhConfig.Sources["testing"] = s
-
-		// viper.Set("AppVersion", 3)
-		// viper.Set("Sources", mhConfig.Sources)
-		// viper.WriteConfig()
 	},
 }
 
