a742bf9f54f00cce5ecbcdfd00cc0988bd9c1fe1
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-05-14 11:55:47 +0200

    Update config content

---
+++ src/config/set.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+func SetDeveloper(name string, email string) error {
+	cfg := Load()
+	dev := Developer{name, email}
+
+	cfg.Developer = dev
+	return update(cfg)
+}
+
+func SetTemplateLocation(loc string) error {
+	cfg := Load()
+	cfg.Templates.Location = loc
+
+	return update(cfg)
+}
+func SetLangDefLocation(loc string) error {
+	cfg := Load()
+	cfg.Languages.Definitions = loc
+
+	return update(cfg)
+}
+
+func update(cfg *Config) error {
+
+	d, err := yaml.Marshal(&cfg)
+
+	if err != nil {
+
+		return err
+	}
+
+	path := filepath.Join(Location(), "config.yaml")
+	err = ioutil.WriteFile(path, d, 0777)
+
+	return err
+
+}
