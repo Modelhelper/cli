921075a2dc4ce6580959bbe0e0c4ce8568d1fc6d
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-05-27 09:47:13 +0200

    Build linux package

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd src
 go get
 
 ## build for windows
-go build -o /c/dev/tools/mh/mh.exe ./main.go
+# go build -o ./bin/win/mh.exe ./main.go
 
 ## build for linux
-# env GOOS=linux go build -o ../bin/linux/mh ./main.go
+env GOOS=linux go build -o ../bin/linux/mh ./main.go
