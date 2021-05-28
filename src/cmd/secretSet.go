6f97e42d061eff0c6f337c01c33e06ac379da935
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-05-28 16:54:43 +0200

    feat: Add secrets to an encrypted vault
    The .secrets file is located in the config folder for global secrets
    or inside a .modelhelper prosjekt folder

---
+++ src/cmd/secretSet.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2021 Hans-Petter Eitvet
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"modelhelper/cli/config"
+	"modelhelper/cli/project"
+	"modelhelper/cli/vault"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// secretSetCmd represents the secretSet command
+var secretSetCmd = &cobra.Command{
+	Use:   "set",
+	Short: "Sets a secret in your secret storage",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		secretsPath := filepath.Join(config.Location(), ".secrets")
+		encodingKey, _ := cmd.Flags().GetString("key")
+		parent := cmd.Parent()
+
+		if parent != nil {
+			// root
+			if parent.Use == "project" {
+				secretsPath = filepath.Join(project.DefaultLocation(), ".secrets")
+			}
+
+		}
+
+		v := vault.File(encodingKey, secretsPath)
+		key, value := args[0], args[1]
+		err := v.Set(key, value)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Value set successfully!")
+	},
+}
+
+func init() {
+	secretCmd.AddCommand(secretSetCmd)
+}
---
+++ src/vault/vault.go
@@ -0,0 +1,88 @@
+package vault
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+	"sync"
+
+	"modelhelper/cli/cipher"
+)
+
+func File(encodingKey, filepath string) *Vault {
+	return &Vault{
+		encodingKey: encodingKey,
+		filepath:    filepath,
+	}
+}
+
+type Vault struct {
+	encodingKey string
+	filepath    string
+	mutex       sync.Mutex
+	keyValues   map[string]string
+}
+
+func (v *Vault) load() error {
+	f, err := os.Open(v.filepath)
+	if err != nil {
+		v.keyValues = make(map[string]string)
+		return nil
+	}
+	defer f.Close()
+	r, err := cipher.DecryptReader(v.encodingKey, f)
+	if err != nil {
+		return err
+	}
+	return v.readKeyValues(r)
+}
+
+func (v *Vault) readKeyValues(r io.Reader) error {
+	dec := json.NewDecoder(r)
+	return dec.Decode(&v.keyValues)
+}
+
+func (v *Vault) save() error {
+	f, err := os.OpenFile(v.filepath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	w, err := cipher.EncryptWriter(v.encodingKey, f)
+	if err != nil {
+		return err
+	}
+	return v.writeKeyValues(w)
+}
+
+func (v *Vault) writeKeyValues(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(v.keyValues)
+}
+
+func (v *Vault) Get(key string) (string, error) {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.load()
+	if err != nil {
+		return "", err
+	}
+	value, ok := v.keyValues[key]
+	if !ok {
+		return "", errors.New("secret: no value for that key")
+	}
+	return value, nil
+}
+
+func (v *Vault) Set(key, value string) error {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.load()
+	if err != nil {
+		return err
+	}
+	v.keyValues[key] = value
+	err = v.save()
+	return err
+}
