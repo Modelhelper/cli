a5457d21a457156ef31fd3c9fb0f20321253a98f
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-06-21 13:21:15 +0200

    Move flag definition to top

--- src/cmd/generate.go
+++ src/cmd/generate.go
@@ -42,6 +42,41 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	rootCmd.AddCommand(generateCmd)
+
+	generateCmd.Flags().StringArrayP("template", "t", []string{}, "A list of template to convert")
+	generateCmd.Flags().StringArray("template-group", []string{}, "Use a group of templates")
+	generateCmd.Flags().String("template-path", "", "Instructs the program to use this path as root for templates")
+
+	generateCmd.Flags().StringP("relations [direct, all, complete]", "r", "", "Include related entities based on the entities in --entity or --entity-group ('direct' | 'all' | 'complete' | 'children' | 'parents')")
+	// generateCmd.Flags().String("template-path", "", "Instructs the program to use this path as root for templates")
+
+	generateCmd.Flags().StringArray("entity-group", []string{}, "Use a group of entities (must be defines in the current connection)")
+	generateCmd.Flags().StringArrayP("entity", "e", []string{}, "A list of entits to use as a model")
+
+	generateCmd.Flags().Bool("screen", false, "List the output to the screen, default false")
+	generateCmd.Flags().Bool("copy", false, "Copies the generated code to the clipboard (ctrl + v), default false")
+	generateCmd.Flags().String("export-path", "", "Exports to a directory")
+	generateCmd.Flags().Bool("export-bykey", false, "Exports the code using the template keys, default false")
+	generateCmd.Flags().Bool("overwrite", false, "Overwrite any existing file when exporting to file on disk")
+
+	generateCmd.Flags().Bool("code-only", false, "Writes only the generated code to the console, no stats, no messages - only code, default false")
+
+	generateCmd.Flags().Bool("demo", false, "Uses a demo as input source, this will override any other input sources (entity, graphql), default false ")
+
+	generateCmd.Flags().String("config-path", "", "Instructs the program to use this config as the config")
+	generateCmd.Flags().String("project-path", "", "Instructs the program to use this project as input")
+
+	generateCmd.Flags().String("key", "", "The key to use when encoding and decoding secrets for a connection")
+
+	// generateCmd.Flags().String("setup", "", "Use this setup to generate code") // version 3.1
+	generateCmd.Flags().StringP("connection", "c", "", "The connection key to be used, uses default connection if not provided")
+
+	generateCmd.RegisterFlagCompletionFunc("relations", completeRelations)
+
+}
+
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:     "generate",
