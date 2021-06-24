1dc373ca8126ff6bf83b37918322860035e155d0
hp-e <hpephoto@gmail.com>
2021-06-24 23:38:59 +0200

    file source

--- src/source/files.go
+++ src/source/files.go
@@ -4,3 +4,19 @@ type Files struct {
 	Connection Connection
 	Path       string
 }
+
+type fileEntity struct {
+	name        string
+	schema      string
+	description string
+	columns     []fileColumn
+}
+
+type fileColumn struct {
+	name        string
+	datatype    string
+	nullable    bool
+	references  string
+	identity    bool
+	description string
+}
