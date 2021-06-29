e6cc335f09f124ad0c34670de6e5a86a0199adda
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-06-29 15:01:18 +0200

    Move slice function to slice package

---
+++ src/slice/string.go
@@ -0,0 +1,13 @@
+package slice
+
+import "strings"
+
+func Contains(s []string, str string) bool {
+	for _, v := range s {
+		if strings.ToLower(v) == strings.ToLower(str) {
+			return true
+		}
+	}
+
+	return false
+}
