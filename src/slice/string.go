69755c50f3ac1667a9a839aff2ad34f45d696ab1
Hans-Petter Eitvet <hans-petter@patogen.no>
2021-06-29 15:05:10 +0200

    Add MaxLen function

--- src/slice/string.go
+++ src/slice/string.go
@@ -2,6 +2,20 @@ package slice
 
 import "strings"
 
+func MaxLen(list []string) int {
+
+	l := 0
+
+	for _, v := range list {
+		tl := len(v)
+		if l < tl {
+			l = tl
+		}
+	}
+
+	return l
+}
+
 func Contains(s []string, str string) bool {
 	for _, v := range s {
 		if strings.ToLower(v) == strings.ToLower(str) {
--- src/slice/strings_test.go
+++ src/slice/strings_test.go
@@ -2,6 +2,7 @@ package slice_test
 
 import (
 	"modelhelper/cli/slice"
+	"modelhelper/cli/strstat"
 	"testing"
 )
 
@@ -21,6 +22,27 @@ func TestContains(t *testing.T) {
 
 		if a != e {
 			t.Errorf("Contains('%s'): Expected %v, got %v", name, e, a)
+		}
+	}
+}
+
+func TestMaxLen(t *testing.T) {
+	type testCase struct {
+		expected int
+		list     []string
+	}
+	tests := []testCase{
+		{expected: 13, list: []string{"one", "two", "three", "max-len-is-13"}},
+		{expected: 3, list: []string{"one", "two"}},
+	}
+
+	for idx, test := range tests {
+
+		a := strstat.MaxLen(test.list)
+		e := test.expected
+
+		if a != e {
+			t.Errorf("MaxLen of array (idx: %d): expected: %v, got %v", idx, e, a)
 		}
 	}
 }
