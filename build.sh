817f9419629f44df8ca341e10ab3b88de6115ecb
hpe <hpephoto@gmail.com>
2021-08-10 09:43:14 +0200

    Set new output path in build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ go get
 # go build -o ./bin/win/mh.exe ./main.go
 
 ## build for linux
-env GOOS=linux go build -o ../bin/linux/mh ./main.go
+# env GOOS=linux go build -o ../bin/linux/mh ./main.go
+env GOOS=linux go build -o ~/dev/tools/mh ./main.go
