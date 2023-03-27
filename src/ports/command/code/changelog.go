56fa250b08a854c4e5829f8e77a099decb578dd0
Hans-Petter Eitvet <hpephoto@gmail.com>
2023-03-27 14:13:02 +0200

    feat(code): Add command to get changelog

---
+++ src/ports/command/code/changelog.go
@@ -0,0 +1,127 @@
+package code
+
+import (
+	"fmt"
+	"modelhelper/cli/modelhelper"
+	"modelhelper/cli/modelhelper/models"
+	"modelhelper/cli/ui"
+	"modelhelper/cli/utils/path"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+func NewChangelogCommand(app *modelhelper.ModelhelperCli) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "changelog",
+		Short: "Reads the commit log and shows as a changelog",
+		Long:  "",
+		Run:   codeChangelogHandler(app),
+	}
+
+	cmd.Flags().StringP("repo", "r", "", "Where the repo is located")
+
+	return cmd
+}
+
+type authorRenderer struct {
+	rows map[string]models.Author
+}
+
+func (d *authorRenderer) Rows() [][]string {
+	var rows [][]string
+
+	for _, e := range d.rows {
+		p := message.NewPrinter(language.English)
+
+		r := []string{
+			e.Name,
+			p.Sprintf("%d", e.Commits),
+			p.Sprintf("%v", e.First.Format("02-Jan-2006")),
+			p.Sprintf("%v", e.Last.Format("02-Jan-2006")),
+		}
+
+		rows = append(rows, r)
+	}
+
+	return rows
+
+}
+
+func (d *authorRenderer) Header() []string {
+	return []string{"Name", "Commits", "First", "Last"}
+}
+
+func codeChangelogHandler(app *modelhelper.ModelhelperCli) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+
+		repo, _ := cmd.Flags().GetString("repo")
+		if len(repo) == 0 {
+			repo = path.CurrentDirectory()
+		}
+		commits, err := app.Code.CommitHistory.GetCommitHistory(repo, nil)
+
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+		}
+
+		commitModel := convertToModel(commits)
+		PrintCommits(commitModel)
+	}
+}
+
+func PrintCommits(commits *models.CommitModel) {
+	printCommitArray("Features", commits.Features)
+	printCommitArray("Fixes", commits.Fixes)
+	printCommitArray("Refactors", commits.Refactors)
+
+	printCommitArray("BREAKING CHANGES", commits.BreakingChanges)
+
+	fmt.Printf("\nAuthors\n\n")
+	table := &authorRenderer{
+		rows: commits.Authors,
+	}
+
+	ui.RenderTable(table)
+	// for au, cnt := range commits.Authors {
+	// 	fmt.Printf("%-20s: commits: %5d, first: %10v, last: %10v\n", au, cnt.Commits, cnt.First.Format("02-Jan-2006"), cnt.Last.Format("02-Jan-2006"))
+	// }
+}
+
+func printCommitArray(title string, list []models.Commit) {
+
+	if len(list) == 0 {
+		return
+	}
+
+	fmt.Printf("%s\n\n", title)
+	for _, cmt := range list {
+		scope := ""
+		if len(cmt.Scope) > 0 {
+			scope = fmt.Sprintf("(%s): ", cmt.Scope)
+		}
+		fmt.Printf("\t* %s%s\n", scope, cmt.Title)
+	}
+}
+
+func convertToModel(in *models.CommitHistory) *models.CommitModel {
+	mdl := &models.CommitModel{}
+
+	mdl.Features = in.Messages["feat"]
+	mdl.Refactors = in.Messages["refactor"]
+	mdl.Fixes = in.Messages["fixes"]
+
+	for _, msg := range in.Messages {
+		for _, commit := range msg {
+			if commit.IsBreakingChange {
+				mdl.BreakingChanges = append(mdl.BreakingChanges, commit)
+			}
+		}
+	}
+
+	mdl.Authors = in.Authors
+	return mdl
+}
--- src/ports/command/connection/create.go
+++ src/ports/command/connection/create.go
@@ -3,6 +3,7 @@ package connection
 import (
 	"fmt"
 	"modelhelper/cli/modelhelper"
+	"modelhelper/cli/modelhelper/models"
 
 	"github.com/spf13/cobra"
 )
@@ -33,10 +34,43 @@ func NewCreateMsSQLConnectionCommand(cs modelhelper.ConnectionService) *cobra.Co
 		Args:  cobra.MinimumNArgs(1),
 		Short: "Creates a MS SQL connection",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Create a file connection")
+			cons, _ := cs.Connections()
+			name := args[0]
+
+			_, exists := cons[name]
+
+			if exists {
+				panic("A connection with the name already exists")
+			}
+
+			con := &models.MsSqlConnection{}
+
+			cs, _ := cmd.Flags().GetString("connection-string")
+
+			if len(cs) > 0 {
+				con.ConnectionString = cs
+
+			} else {
+
+				s, _ := cmd.Flags().GetString("server")
+				d, _ := cmd.Flags().GetString("database")
+				u, _ := cmd.Flags().GetString("user")
+				p, _ := cmd.Flags().GetString("password")
+
+				if len(s) > 0 && len(d) > 0 && len(u) > 0 && len(p) > 0 {
+					con.Database = d
+					con.Server = s
+					con.ConnectionString = fmt.Sprintf("sqlserver://%s:%s@%s?database=%s", u, p, s, d)
+				}
+			}
 		},
 	}
 
+	cmd.Flags().StringP("connection-string", "c", "", "The full connection string to use")
+	cmd.Flags().StringP("server", "s", "", "The server")
+	cmd.Flags().StringP("database", "d", "", "The database")
+	cmd.Flags().StringP("username", "u", "", "The user")
+	cmd.Flags().StringP("password", "p", "", "The password")
 	return cmd
 }
 
