82962075670b873ccab75fc63a308f510ad66150
Hans-Petter Eitvet <hpephoto@gmail.com>
2024-11-07 17:28:53 +0100

    refactor: Add options for ConfigService

--- modelhelper/ports-config.go
+++ modelhelper/ports-config.go
@@ -8,8 +8,11 @@ type CreateConfigService interface {
 	Create() *models.Config
 }
 type ConfigService interface {
+	ConfigExists() bool
+	Create() (*models.Config, error)
 	Load() (*models.Config, error)
 	Save() error
+	SaveConfig(c *models.Config) error
 	LoadFromFile(path string) (*models.Config, error)
 }
 
