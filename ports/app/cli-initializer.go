1bfc0d39ef54435805e2548be18d1a8db6c6ad04
Hans-Petter Eitvet <hpephoto@gmail.com>
2024-11-07 17:26:44 +0100

    refactor: Improve cli initializer

--- ports/app/cli-initializer.go
+++ ports/app/cli-initializer.go
@@ -3,15 +3,18 @@ package app
 import (
 	"context"
 	"modelhelper/cli/modelhelper"
+
+	"github.com/charmbracelet/huh"
 )
 
 type appInitializer struct {
-	info modelhelper.AppInfoService
-	cfg  modelhelper.ConfigService
+	info        modelhelper.AppInfoService
+	cfg         modelhelper.ConfigService
+	connService modelhelper.ConnectionService
 }
 
-func NewCliInitializer(ctx context.Context, info modelhelper.AppInfoService, cfg modelhelper.ConfigService) modelhelper.AppInitializer {
-	return &appInitializer{info, cfg}
+func NewCliInitializer(ctx context.Context, info modelhelper.AppInfoService, cfg modelhelper.ConfigService, cs modelhelper.ConnectionService) modelhelper.AppInitializer {
+	return &appInitializer{info, cfg, cs}
 }
 
 func (ai *appInitializer) IsInitialized() bool {
@@ -19,6 +22,24 @@ func (ai *appInitializer) IsInitialized() bool {
 }
 
 func (ai *appInitializer) Initialize() error {
+	var (
+		codeEditor string
+	)
+	dbTypeSelect := huh.NewSelect[string]().
+		Title("It looks like this is the first time the ModelHelper is run on this computer").
+		Description("Please select the type of editor you want to connect use to view things in").
+		Options(
+			huh.NewOption("I do not want to set a default one", "none"),
+			huh.NewOption("Visual Studio Code (code)", "code"),
+			huh.NewOption("Vim", "vim"),
+			huh.NewOption("Nano", "nano"),
+		).
+		Value(&codeEditor)
+
+	huh.NewForm(
+		huh.NewGroup(dbTypeSelect),
+		// huh.NewGroup(ringSelect),
+	).Run()
 
 	return nil
 }
