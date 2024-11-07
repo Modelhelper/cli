df6d321d8810050b009ab3158f0dbaa02c28dba1
Hans-Petter Eitvet <hpephoto@gmail.com>
2024-11-07 17:28:04 +0100

    refactor: Edit command for code template

--- ports/command/code/template-commands.go
+++ ports/command/code/template-commands.go
@@ -22,6 +22,7 @@ func NewTemplatesCommand(app *modelhelper.ModelhelperCli) *cobra.Command {
 
 	cmd.AddCommand(NewTemplateListCommand(app))
 	cmd.AddCommand(templateDetailCommand(app))
+	cmd.AddCommand(NewEditCodeTemplateCommand(app))
 
 	return cmd
 }
@@ -43,3 +44,21 @@ func templateDetailCommand(app *modelhelper.ModelhelperCli) *cobra.Command {
 
 	return cmd
 }
+
+// func templateDetailCommand(app *modelhelper.ModelhelperCli) *cobra.Command {
+// 	cmd := &cobra.Command{
+// 		Use:   "detail [name]",
+// 		Args:  cobra.ExactArgs(1),
+// 		Short: "Get details about a named template",
+
+// 		Run: func(cmd *cobra.Command, args []string) {
+
+// 			fmt.Println("mh project template detail")
+// 			tpl := app.Project.TemplateService.Load(args[0])
+
+// 			fmt.Printf("name: %s, lang: %s", tpl.Name, tpl.Language)
+// 		},
+// 	}
+
+// 	return cmd
+// }
